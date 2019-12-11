1db7239a79a47afe49c291f70120ff1ea9ebedb2
Jerimiah Gazlay <jgazlay@lyft.com>
2019-12-10 22:31:45 -0600

    day-07 part1

---
+++ day-07/input.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using PuzzleInput;
+
+
+namespace PuzzleInput
+{
+    class Input
+    {
+        public static Dictionary<string, int[]> TestInputPart1 = new Dictionary<string, int[]> {
+            // output = 43210
+            {"memory", new int[] {3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0}},
+            {"phases", new int[] {4,3,2,1,0}},
+        };
+        public static Dictionary<string, int[]> TestInputPart2 = new Dictionary<string, int[]> {
+            // output = 54321
+            {"memory", new int[] {3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0}},
+            {"phases", new int[] {0,1,2,3,4}},
+        };
+        public static Dictionary<string, int[]> TestInputPart3 = new Dictionary<string, int[]> {
+            // output = 65210
+            {"memory", new int[] {3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0}},
+            {"phases", new int[] {1,0,4,3,2}},
+        };
+        public static int[] PuzzleInput = new int[] {3,8,1001,8,10,8,105,1,0,0,21,34,59,68,89,102,183,264,345,426,99999,3,9,102,5,9,9,1001,9,5,9,4,9,99,3,9,101,3,9,9,1002,9,5,9,101,5,9,9,1002,9,3,9,1001,9,5,9,4,9,99,3,9,101,5,9,9,4,9,99,3,9,102,4,9,9,101,3,9,9,102,5,9,9,101,4,9,9,4,9,99,3,9,1002,9,5,9,1001,9,2,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99};
+        public static string[] allPhases = new PermutePhases().Run("01234");
+    }
+}
+
+
+class PermutePhases
+{ // thanks to https://www.geeksforgeeks.org/c-program-to-print-all-permutations-of-a-given-string-2/ for the help on this one
+    /**
+    * permutation function
+    * @param str string to
+       calculate permutation for
+    * @param l starting index
+    * @param r end index
+    */
+    public static List<string> permutations = new List<string>();
+    private static void permute(String str,
+                                int l, int r)
+    {
+        if (l == r)
+        {
+            Console.WriteLine(str);
+            permutations.Add(str);
+        }
+        else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                permute(str, l + 1, r);
+                str = swap(str, l, i);
+            }
+        }
+    }
+
+    /**
+    * Swap Characters at position
+    * @param a string value
+    * @param i position 1
+    * @param j position 2
+    * @return swapped string
+    */
+    public static String swap(String a,
+                              int i, int j)
+    {
+        char temp;
+        char[] charArray = a.ToCharArray();
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        string s = new string(charArray);
+        return s;
+    }
+
+    public string[] Run(string str)
+    {
+        int n = str.Length;
+        permute(str, 0, n - 1);
+        return permutations.ToArray();
+    }
+}
---
+++ day-07/part1.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Linq;
+using PuzzleInput;
+
+class Intcode
+{
+    private static int TERMINATE_OPCODE = 99;
+    private static int ADD_OPCODE = 1;
+    private static int MULTIPLY_OPCODE = 2;
+    private static int INPUT_OPCODE = 3;
+    private static int OUTPUT_OPCODE = 4;
+    private static int JUMP_IF_TRUE_OPCODE = 5;
+    private static int JUMP_IF_FALSE_OPCODE = 6;
+    private static int LESS_THAN_OPCODE = 7;
+    private static int EQUALS_OPCODE = 8;
+
+    private bool phaseSettingUsed = false;
+    private int[] memory;
+    private int output;
+
+    public Intcode(int[] memory)
+    {
+        this.memory = memory;
+    }
+
+    public int Run(int input, int phaseSetting)
+    {
+        int instructionPointer = 0;
+        int[] instructions = new int[memory.Length];
+        int opcode;
+        string parameterModes;
+        int instructionSet = 4;
+        Array.Copy(memory, instructions, memory.Length);
+
+        while (instructions[instructionPointer] != TERMINATE_OPCODE)
+        {
+            opcode = instructions[instructionPointer] % 10;
+            parameterModes = (instructions[instructionPointer] / 100).ToString();
+            if (parameterModes.Length == 1)
+                parameterModes = "0" + parameterModes;
+
+            if (opcode == ADD_OPCODE)
+            {
+                instructions[instructions[instructionPointer + 3]] = GetValue(instructions, instructionPointer + 1, parameterModes[1]) + GetValue(instructions, instructionPointer + 2, parameterModes[0]);
+                instructionSet = 4;
+            }
+            else if (opcode == MULTIPLY_OPCODE)
+            {
+                instructions[instructions[instructionPointer + 3]] = GetValue(instructions, instructionPointer + 1, parameterModes[1]) * GetValue(instructions, instructionPointer + 2, parameterModes[0]);
+                instructionSet = 4;
+            }
+            else if (opcode == INPUT_OPCODE)
+            {
+                int value = phaseSettingUsed ? input : phaseSetting;
+                phaseSettingUsed = true;
+                instructions[instructions[instructionPointer + 1]] = value;
+                instructionSet = 2;
+            }
+            else if (opcode == OUTPUT_OPCODE)
+            {
+                Console.WriteLine("output: {0}", GetValue(instructions, instructionPointer + 1, parameterModes[1]).ToString());
+                output = GetValue(instructions, instructionPointer + 1, parameterModes[1]);
+                instructionSet = 2;
+            }
+            else if (opcode == JUMP_IF_TRUE_OPCODE)
+            {
+                if (GetValue(instructions, instructionPointer + 1, parameterModes[1]) != 0)
+                {
+                    int value = GetValue(instructions, instructionPointer + 2, parameterModes[0]);
+                    instructions[instructionPointer] = value;
+                    instructionPointer = value;
+                    instructionSet = 0;
+                } else
+                    instructionSet = 3;
+            }
+            else if (opcode == JUMP_IF_FALSE_OPCODE)
+            {
+                if (GetValue(instructions, instructionPointer + 1, parameterModes[1]) == 0)
+                {
+                    int value = GetValue(instructions, instructionPointer + 2, parameterModes[0]);
+                    instructions[instructionPointer] = value;
+                    instructionPointer = value;
+                    instructionSet = 0;
+                } else
+                    instructionSet = 3;
+            }
+            else if (opcode == LESS_THAN_OPCODE)
+            {
+                bool isLessThan = GetValue(instructions, instructionPointer + 1, parameterModes[1]) < GetValue(instructions, instructionPointer + 2, parameterModes[0]);
+                instructions[instructions[instructionPointer + 3]] = isLessThan ? 1 : 0;
+                instructionSet = 4;
+            }
+            else if (opcode == EQUALS_OPCODE)
+            {
+                bool isEqual = GetValue(instructions, instructionPointer + 1, parameterModes[1]) == GetValue(instructions, instructionPointer + 2, parameterModes[0]);
+                instructions[instructions[instructionPointer + 3]] = isEqual ? 1 : 0;
+                instructionSet = 4;
+            }
+            else if (opcode != 99)
+                return 0;
+
+            Console.WriteLine("instructionPointer: {0} opcode: {1} instructionSet: {2}", instructionPointer, opcode, instructionSet);
+            instructionPointer += instructionSet;
+        }
+        phaseSettingUsed = false;
+        return output;
+    }
+
+    private static int GetValue(int[] instructions, int position, char parameterMode)
+    {
+        if (parameterMode == Convert.ToChar("1"))
+            return instructions[position];
+
+        return instructions[instructions[position]];
+    }
+}
+
+
+class GravityAssist
+{
+    static void Main()
+    {
+        int[] memory = Input.PuzzleInput;
+        string[] phaseSequences = Input.allPhases;
+        int maxOutput = 0;
+
+        Intcode ampA = new Intcode(memory);
+        Intcode ampB = new Intcode(memory);
+        Intcode ampC = new Intcode(memory);
+        Intcode ampD = new Intcode(memory);
+        Intcode ampE = new Intcode(memory);
+
+        foreach (string phaseSequence in phaseSequences)
+        {
+            int[] phaseSettings = Array.ConvertAll(phaseSequence.ToCharArray(), x => int.Parse(x.ToString()));
+
+            int outputA = ampA.Run(0, phaseSettings[0]);
+            int outputB = ampB.Run(outputA, phaseSettings[1]);
+            int outputC = ampC.Run(outputB, phaseSettings[2]);
+            int outputD = ampD.Run(outputC, phaseSettings[3]);
+            int finalOutput = ampE.Run(outputD, phaseSettings[4]);
+
+            maxOutput = Math.Max(maxOutput, finalOutput);
+        }
+
+
+
+        Console.WriteLine(maxOutput);
+    }
+}
